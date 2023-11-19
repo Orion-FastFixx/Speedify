db17e3e10cf32014f4b5c227a6322926f080d845
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-20 01:22:33 +0700

    Dependency injection

--- app/src/main/java/com/example/speedify/di/SpeedifyModule.kt
+++ app/src/main/java/com/example/speedify/di/SpeedifyModule.kt
@@ -1,4 +1,32 @@
 package com.example.speedify.di
 
+import com.example.speedify.feature_bengkel.data.repository.BengkelRepositoryImpl
+import com.example.speedify.feature_bengkel.domain.interface_repository.BengkelRepository
+import com.example.speedify.feature_bengkel.domain.use_case.GetAllBengkelMobil
+import com.example.speedify.feature_bengkel.domain.use_case.GetAllPromotion
+import com.example.speedify.feature_bengkel.domain.use_case.UseCasesBengkel
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object SpeedifyModule {
+
+    @Provides
+    @Singleton
+    fun provideBengkelRepository(): BengkelRepository {
+        return BengkelRepositoryImpl.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBengkelUseCases(repository: BengkelRepository): UseCasesBengkel {
+        return UseCasesBengkel(
+            getAllPromotion = GetAllPromotion(repository),
+            getAllBengkelMobil = GetAllBengkelMobil(repository)
+        )
+    }
 }
