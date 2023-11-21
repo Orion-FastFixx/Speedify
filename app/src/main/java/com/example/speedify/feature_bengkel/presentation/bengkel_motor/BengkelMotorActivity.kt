bf94ccebfeac76599ecc5be7d2965f977a0ce4be
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-21 16:42:29 +0700

    feat(bengke motor screen):  show all bengkel motor

---
+++ app/src/main/java/com/example/speedify/feature_bengkel/presentation/bengkel_motor/BengkelMotorActivity.kt
@@ -0,0 +1,54 @@
+package com.example.speedify.feature_bengkel.presentation.bengkel_motor
+
+import android.content.ContentValues
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.speedify.databinding.ActivityBengkelMotorBinding
+import com.example.speedify.feature_bengkel.presentation.bengkel_motor.adapter.BengkelMotorAdapter
+import com.example.speedify.feature_bengkel.presentation.bengkel_motor.view_model.BengkelMotorViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BengkelMotorActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityBengkelMotorBinding
+
+    private val binding get() = _binding
+
+    private val viewModel: BengkelMotorViewModel by viewModels()
+
+    private val bengkelMotorAdapter by lazy {
+        BengkelMotorAdapter()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityBengkelMotorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val state = viewModel.bengkelMotorState.value
+
+        initAdapter()
+
+        if (state.isLoading) {
+            Log.d(ContentValues.TAG, "BengkelMotor:   Loading")
+        } else if (state.error != null) {
+            Log.e(ContentValues.TAG, "BengkelMotor:   ${state.error}")
+        } else {
+            state.bengkelMotor?.let {
+                bengkelMotorAdapter.setItems(it)
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        binding.rvBengkelMotor.apply {
+            adapter = bengkelMotorAdapter
+            layoutManager =
+                LinearLayoutManager(this@BengkelMotorActivity, LinearLayoutManager.VERTICAL, false)
+        }
+    }
+}
