899ff6363bebd688465170d583e42e8c92d1fd87
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-21 10:59:09 +0700

    refactor: rename bengkel home vm

--- app/src/main/java/com/example/speedify/feature_bengkel/presentation/home/BengkelHomeState.kt
+++ app/src/main/java/com/example/speedify/feature_bengkel/presentation/home/BengkelHomeState.kt
@@ -3,7 +3,7 @@ package com.example.speedify.feature_bengkel.presentation.home
 import com.example.speedify.feature_bengkel.domain.entity.BengkelEntity
 import com.example.speedify.feature_bengkel.domain.entity.PromotionEntity
 
-data class BengkelState(
+data class BengkelHomeState(
     val isLoading: Boolean = true,
     val error: String? = null,
     val promotion: List<PromotionEntity>? = null,
