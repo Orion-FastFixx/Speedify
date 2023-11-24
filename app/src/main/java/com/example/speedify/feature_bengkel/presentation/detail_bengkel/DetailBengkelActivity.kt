95469d2fdbc31b16071a3876cd311bc20434436a
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-24 23:25:20 +0700

    feat(detail bengkel screen): wip detail bengkel

---
+++ app/src/main/java/com/example/speedify/feature_bengkel/presentation/detail_bengkel/DetailBengkelActivity.kt
@@ -0,0 +1,12 @@
+package com.example.speedify.feature_bengkel.presentation.detail_bengkel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.speedify.R
+
+class DetailBengkelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_bengkel)
+    }
+}
