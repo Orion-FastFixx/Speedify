49345853610d8a9cd262fc9d618325a0c19a2aad
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-29 23:49:33 +0700

    feat(payment methods): add payment method screen v1

---
+++ app/src/main/java/com/example/speedify/feature_bengkel/presentation/checkout_bengkel/PaymentMethodsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.speedify.feature_bengkel.presentation.checkout_bengkel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.speedify.R
+
+class PaymentMethodsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_methods)
+    }
+}
