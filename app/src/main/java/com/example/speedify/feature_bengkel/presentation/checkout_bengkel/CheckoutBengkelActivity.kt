8480d13ac066c7302a42ec56a7cd6d5599d9a4e7
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-30 01:09:08 +0700

    feat(check out bengkel): configure section text

--- app/src/main/java/com/example/speedify/feature_bengkel/presentation/checkout_bengkel/CheckoutBengkelActivity.kt
+++ app/src/main/java/com/example/speedify/feature_bengkel/presentation/checkout_bengkel/CheckoutBengkelActivity.kt
@@ -3,6 +3,7 @@ package com.example.speedify.feature_bengkel.presentation.checkout_bengkel
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.speedify.R
 import com.example.speedify.databinding.ActivityCheckoutBengkelBinding
 
 class CheckoutBengkelActivity : AppCompatActivity() {
@@ -15,7 +16,24 @@ class CheckoutBengkelActivity : AppCompatActivity() {
         _binding = ActivityCheckoutBengkelBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val morePayment = binding.icArrowRightPaymentMethod
+        dynamicConfiguration()
+    }
+
+    private fun dynamicConfiguration() {
+//      Set text order summary
+        val orderSummary = binding.checkoutBengkel.orderSummary.tvSectionName
+        orderSummary.text = getString(R.string.order_summary)
+
+//      Set text location
+        val location = binding.checkoutBengkel.location.tvSectionName
+        location.text = getString(R.string.section_location)
+
+//      Set text payment method
+        val paymentMethod = binding.checkoutBengkel.paymentMethod.tvSectionName
+        paymentMethod.text = getString(R.string.payment_method)
+
+//      Go to Payment Methods
+        val morePayment = binding.checkoutBengkel.icArrowRightPaymentMethod
         morePayment.setOnClickListener {
             val intent = Intent(this, PaymentMethodsActivity::class.java)
             startActivity(intent)
