4e4543fc4004f9ba4b0b611b19e07c0eca0f9140
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-30 21:16:59 +0700

    feat(detail screen): detail screen view v2

--- app/src/main/java/com/example/speedify/feature_bengkel/presentation/detail_bengkel/DetailBengkelActivity.kt
+++ app/src/main/java/com/example/speedify/feature_bengkel/presentation/detail_bengkel/DetailBengkelActivity.kt
@@ -1,12 +1,30 @@
 package com.example.speedify.feature_bengkel.presentation.detail_bengkel
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.speedify.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.speedify.databinding.ActivityDetailBengkelBinding
+import com.example.speedify.feature_bengkel.presentation.checkout_bengkel.CheckoutBengkelActivity
 
 class DetailBengkelActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityDetailBengkelBinding
+
+    private val binding get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_bengkel)
+        _binding = ActivityDetailBengkelBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        dynamicConfiguration()
+    }
+
+    private fun dynamicConfiguration() {
+//      Go to Checkout Bengkel
+        val placeOrder = binding.btnPlaceOrderDetailBengkel
+        placeOrder.setOnClickListener {
+            val intent = Intent(this, CheckoutBengkelActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
